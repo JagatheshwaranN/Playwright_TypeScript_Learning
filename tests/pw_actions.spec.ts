dcc7094d70c36a5e4b68188c493c2dfa8f38585d
Jagatheshwaran N <jagatheshwaran.n29@gmail.com>
2026-02-01 15:28:06 -0600

    PW Actions & Dropdown Updated

--- tests/pw_actions.spec.ts
+++ tests/pw_actions.spec.ts
@@ -1,83 +1,103 @@
-import {test, expect, Locator} from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
 test.describe('PW Actions', () => {
-    test.beforeEach(async ({page}) => {
-        await page.goto('https://testautomationpractice.blogspot.com/');
-    })
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://testautomationpractice.blogspot.com/');
+  })
 
-    test('Text Input Actions - Get Attribute', async ({page}) => {
-       const textInput: Locator = page.locator('#name');
-         await expect(textInput).toBeVisible();
-         await expect(textInput).toBeEnabled();
-         await textInput.getAttribute('maxlength').then(value => {
-            console.log("Max Length Attribute:", value);
-         });
-    }); 
-
-    test('Text Input Actions - Get Input Value', async ({page}) => {
-        const fitstNameInput:Locator = page.locator("#name");
-        await expect(fitstNameInput).toBeVisible();
-        await fitstNameInput.fill("Playwright");
-        const inputValue = await fitstNameInput.inputValue();
-        console.log("Input Value:", inputValue);
-        expect(inputValue).toBe("Playwright");
+  test('Text Input Actions - Get Attribute', async ({ page }) => {
+    const textInput: Locator = page.locator('#name');
+    await expect(textInput).toBeVisible();
+    await expect(textInput).toBeEnabled();
+    await textInput.getAttribute('maxlength').then(value => {
+      console.log("Max Length Attribute:", value);
     });
+  });
+
+  test('Text Input Actions - Get Input Value', async ({ page }) => {
+    const fitstNameInput: Locator = page.locator("#name");
+    await expect(fitstNameInput).toBeVisible();
+    await fitstNameInput.fill("Playwright");
+    const inputValue = await fitstNameInput.inputValue();
+    console.log("Input Value:", inputValue);
+    expect(inputValue).toBe("Playwright");
+  });
+
+  test('Text Input Actions - Get Input Value by Locate Element Approach 2', async ({ page }) => {
+    const fitstNameInput = await page.$("#name");
+    await fitstNameInput?.fill("Playwright");
+    const inputValue = await fitstNameInput?.inputValue();
+    console.log("Input Value:", inputValue);
+    expect(inputValue).toBe("Playwright");
+  });
+
+    test('Text Input Actions - Clear Input Value', async ({ page }) => {
+    const fitstNameInput = await page.$("#name");
+    await fitstNameInput?.fill("Playwright");
+    const inputValue = await fitstNameInput?.inputValue();
+    console.log("Input Value:", inputValue);
+    expect(inputValue).toBe("Playwright");
+    await fitstNameInput?.fill("");
+    const clearedValue = await fitstNameInput?.inputValue();
+    console.log("Cleared Input Value:", clearedValue);
+    expect(clearedValue).toBe("");
+  });
 
 
-    test('Radio Button Actions - Check Selected State', async ({page}) => {
-        const maleRadio: Locator = page.locator('#male');
-        await expect(maleRadio).toBeVisible();
-        await expect(maleRadio).toBeEnabled();
-        const isSelectedBefore = await maleRadio.isChecked();
-        console.log("Is Male Radio Selected Before Click:", isSelectedBefore);
-        await maleRadio.check();
-        const isSelectedAfter = await maleRadio.isChecked();
-        console.log("Is Male Radio Selected After Click:", isSelectedAfter);
-        expect(isSelectedAfter).toBe(true);
-    });
+  test('Radio Button Actions - Check Selected State', async ({ page }) => {
+    const maleRadio: Locator = page.locator('#male');
+    await expect(maleRadio).toBeVisible();
+    await expect(maleRadio).toBeEnabled();
+    const isSelectedBefore = await maleRadio.isChecked();
+    console.log("Is Male Radio Selected Before Click:", isSelectedBefore);
+    await maleRadio.check();
+    const isSelectedAfter = await maleRadio.isChecked();
+    console.log("Is Male Radio Selected After Click:", isSelectedAfter);
+    expect(isSelectedAfter).toBe(true);
+  });
 
-    test.only('Checkbox Actions - Check Selected State', async ({page}) => {
-        const sundayCheckbox: Locator = page.getByLabel('Sunday');
-        await expect(sundayCheckbox).toBeVisible();
-        await expect(sundayCheckbox).toBeEnabled();
-        const isCheckedBefore = await sundayCheckbox.isChecked();
-        console.log("Is Sunday Checkbox Selected Before Click:", isCheckedBefore);
-        await sundayCheckbox.check();
-        const isCheckedAfter = await sundayCheckbox.isChecked();
-        console.log("Is Sunday Checkbox Selected After Click:", isCheckedAfter);
-        await expect(sundayCheckbox).toBeChecked();
-        expect(isCheckedAfter).toBe(true);
+  test('Checkbox Actions - Check Selected State', async ({ page }) => {
+    const sundayCheckbox: Locator = page.getByLabel('Sunday');
+    await expect(sundayCheckbox).toBeVisible();
+    await expect(sundayCheckbox).toBeEnabled();
+    const isCheckedBefore = await sundayCheckbox.isChecked();
+    console.log("Is Sunday Checkbox Selected Before Click:", isCheckedBefore);
+    await sundayCheckbox.check();
+    const isCheckedAfter = await sundayCheckbox.isChecked();
+    console.log("Is Sunday Checkbox Selected After Click:", isCheckedAfter);
+    await expect(sundayCheckbox).toBeChecked();
+    expect(isCheckedAfter).toBe(true);
 
-        const days:string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        for(const day of days) {
-          const dayCheckbox:Locator = page.getByLabel(day);
-          const isDayCheckedBefore = await dayCheckbox.isChecked();
-          console.log(`Is ${day} Checkbox Selected Before Click:`, isDayCheckedBefore);
-          await dayCheckbox.check();
-          const isDayCheckedAfter = await dayCheckbox.isChecked();
-          console.log(`Is ${day} Checkbox Selected After Click:`, isDayCheckedAfter);
-          await expect(dayCheckbox).toBeChecked();
-          expect(isDayCheckedAfter).toBe(true);
-        }
-        
-        const daysToMap:string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const checkboxes:Locator[] = daysToMap.map(index => page.getByLabel(index));
+    const days: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for (const day of days) {
+      const dayCheckbox: Locator = page.getByLabel(day);
+      const isDayCheckedBefore = await dayCheckbox.isChecked();
+      console.log(`Is ${day} Checkbox Selected Before Click:`, isDayCheckedBefore);
+      await dayCheckbox.check();
+      const isDayCheckedAfter = await dayCheckbox.isChecked();
+      console.log(`Is ${day} Checkbox Selected After Click:`, isDayCheckedAfter);
+      await expect(dayCheckbox).toBeChecked();
+      expect(isDayCheckedAfter).toBe(true);
+    }
 
-        for(const checkbox of checkboxes.slice(-3)) {
-          await checkbox.uncheck();
-          expect(checkbox).not.toBeChecked();
-        }
+    const daysToMap: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const checkboxes: Locator[] = daysToMap.map(index => page.getByLabel(index));
 
-        for(const checkbox of checkboxes) {
-          if(await checkbox.isChecked()) {
-            await checkbox.uncheck();
-            expect(checkbox).not.toBeChecked();
-          } else {
-            await checkbox.check();
-            expect(checkbox).toBeChecked();
-          }
-        }
-        await page.waitForTimeout(5000);
-    });
+    for (const checkbox of checkboxes.slice(-3)) {
+      await checkbox.uncheck();
+      expect(checkbox).not.toBeChecked();
+    }
+
+    for (const checkbox of checkboxes) {
+      if (await checkbox.isChecked()) {
+        await checkbox.uncheck();
+        expect(checkbox).not.toBeChecked();
+      } else {
+        await checkbox.check();
+        expect(checkbox).toBeChecked();
+      }
+    }
+    await page.waitForTimeout(5000);
+  });
 
 });
--- tests/pw_dropdown.spec.ts
+++ tests/pw_dropdown.spec.ts
@@ -1,73 +1,80 @@
-import {test, expect, Locator} from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
 test.describe('PW Dropdown Actions', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://letcode.in/dropdowns');
-  });   
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://letcode.in/dropdowns');
+    });
 
 
-    test('Normal / Single Select Dropdown', async ({ page }) => {  
-        const dropdown:Locator = page.locator('#fruits');
+    test('Normal / Single Select Dropdown', async ({ page }) => {
+        const dropdown: Locator = page.locator('#fruits');
         await expect(dropdown).toBeVisible();
         await dropdown.selectOption('Apple');
-       
+
         await dropdown.selectOption({ label: 'Orange' });
 
-        await dropdown.selectOption({ value: '1'});
+        await dropdown.selectOption({ value: '1' });
 
         await dropdown.selectOption({ index: 4 });
-    
-        const dropdownOptions:Locator = dropdown.locator('option');
+
+        const dropdownOptions: Locator = dropdown.locator('option');
         const optionsCount = await dropdownOptions.count();
         console.log("Total Options in Dropdown:", optionsCount);
         await expect(dropdownOptions).toHaveCount(6);
 
-        const optionTexts:string[] = await dropdownOptions.allTextContents();
+        const optionTexts: string[] = await dropdownOptions.allTextContents();
         console.log("Dropdown Options Texts:", optionTexts);
         expect(optionTexts).toContain('Orange');
 
-        const optionsValues:string[] = [];
-       for(let i=0; i<optionsCount;i++){
-        const optionValue = await dropdownOptions.nth(i).getAttribute('value');
-        optionsValues.push(optionValue? optionValue : '');
-       }
-       console.log("Dropdown Options Values:", optionsValues);
+        const optionsValues: string[] = [];
+        for (let i = 0; i < optionsCount; i++) {
+            const optionValue = await dropdownOptions.nth(i).getAttribute('value');
+            optionsValues.push(optionValue ? optionValue : '');
+        }
+        console.log("Dropdown Options Values:", optionsValues);
 
-       await page.waitForTimeout(2000);
+        await page.waitForTimeout(2000);
 
     });
 
-    test('Multi Select Dropdown', async ({page})=> {
-        const multiSelectDropdown:Locator = page.locator('#superheros');
+    test('Multi Select Dropdown', async ({ page }) => {
+        const multiSelectDropdown: Locator = page.locator('#superheros');
         await expect(multiSelectDropdown).toBeVisible();
-        await multiSelectDropdown.selectOption([{label:'Aquaman'}, {value:'bt'}, {index: 0}]);
+        await multiSelectDropdown.selectOption([{ label: 'Aquaman' }, { value: 'bt' }, { index: 0 }]);
         await page.waitForTimeout(2000);
     })
-   
-    test('Dropdown Sorted Check', async ({page}) => {
-        const dropdown:Locator = page.locator('#fruits > option');
-        const optionsText:string[] = await dropdown.allTextContents();
-        const originalOptionsText:string[] = [...optionsText];
-        const sortedOptionsText:string[] = [...optionsText].sort();
+
+    test('Get Selected Dropdown Value using $eval', async ({ page }) => {
+        await page.selectOption('#country', 'India');   
+        const text = await page.$eval<string, HTMLSelectElement>('#country', el => (el as HTMLSelectElement).value);
+        console.log("Selected Country Value:", text);
+        expect(text).toBe('India');
+    })
+
+    test('Dropdown Sorted Check', async ({ page }) => {
+        const dropdown: Locator = page.locator('#fruits > option');
+        const optionsText: string[] = await dropdown.allTextContents();
+        const originalOptionsText: string[] = [...optionsText];
+        const sortedOptionsText: string[] = [...optionsText].sort();
         console.log("Dropdown Options Texts - Original Order:", originalOptionsText);
         console.log("Dropdown Options Texts - Sorted Order:", sortedOptionsText);
         expect(originalOptionsText).not.toEqual(sortedOptionsText);
     })
 
-    test('Check Duplicate Options in Dropdown', async ({page}) => {
-        const dropdown:Locator = page.locator('#fruits > option');
-        const optionsText:string[] = await dropdown.allTextContents();
-        const duplicateOptions:string[] = [];
+    test('Check Duplicate Options in Dropdown', async ({ page }) => {
+        const dropdown: Locator = page.locator('#fruits > option');
+        const optionsText: string[] = await dropdown.allTextContents();
+        const duplicateOptions: string[] = [];
         const optionsSet = new Set<string>();
-        for(let options of optionsText) {
-            if(optionsSet.has(options)) {
+        for (let options of optionsText) {
+            if (optionsSet.has(options)) {
                 duplicateOptions.push(options);
-            }else{
+            } else {
                 optionsSet.add(options);
             }
         }
         console.log("Duplicate Options in Dropdown:", duplicateOptions);
-        if(duplicateOptions.length > 0) {
+        if (duplicateOptions.length > 0) {
             console.log("There are duplicate options in the dropdown.");
         } else {
             console.log("No duplicate options found in the dropdown.");
@@ -76,36 +83,36 @@ test.describe('PW Dropdown Actions', () => {
 });
 
 test.describe.only('Handling Custom Dropdowns', () => {
-   
-    test('Auto Suggestion Dropdown', async ({page}) => {
+
+    test('Auto Suggestion Dropdown', async ({ page }) => {
         await page.goto('https://www.amazon.com/');
-        const autoSuggestInput:Locator = page.locator('#twotabsearchtextbox');
+        const autoSuggestInput: Locator = page.locator('#twotabsearchtextbox');
         await expect(autoSuggestInput).toBeVisible();
         await autoSuggestInput.fill('smart');
         await page.waitForTimeout(2000);
-        const autoSuggestOptions:Locator = page.locator('div.s-suggestion');
+        const autoSuggestOptions: Locator = page.locator('div.s-suggestion');
         const optionsCount = await autoSuggestOptions.count();
         console.log("Total Auto-Suggest Options:", optionsCount);
-        for(let i=1; i<=optionsCount; i++) {
+        for (let i = 1; i <= optionsCount; i++) {
             const optionsText = await page.locator(`//div[@id="sac-suggestion-row-${i}"]`).textContent();
             console.log(`Option ${i}:`, optionsText?.trim());
         }
         await page.waitForTimeout(2000);
     })
 
-    test.only('Custom Dropdown - Browser Selection', async ({page}) => {
+    test.only('Custom Dropdown - Browser Selection', async ({ page }) => {
         await page.goto('file:///D:/Environments/VisualStudio/Playwright_Learning/tests/files/Dropdown.html');
-        const dropdownTrigger:Locator = page.locator('.custom-select');
+        const dropdownTrigger: Locator = page.locator('.custom-select');
         await expect(dropdownTrigger).toBeVisible();
         await dropdownTrigger.click();
-        const dropdownOptions :Locator = page.locator('.select-items');
+        const dropdownOptions: Locator = page.locator('.select-items');
         await expect(dropdownOptions).toBeVisible();
-        const dropdownOptionsTexts:string[] = await dropdownOptions.locator('li').allTextContents(); 
+        const dropdownOptionsTexts: string[] = await dropdownOptions.locator('li').allTextContents();
         console.log("Custom Dropdown Options:", dropdownOptionsTexts);
         const optionToSelect = 'Safari';
-        for(const optionText of dropdownOptionsTexts) {
-            if(optionText === optionToSelect) {
-                await dropdownOptions.locator('li', {hasText: optionText}).click();
+        for (const optionText of dropdownOptionsTexts) {
+            if (optionText === optionToSelect) {
+                await dropdownOptions.locator('li', { hasText: optionText }).click();
                 console.log(`Selected Option: ${optionText}`);
                 break;
             }
@@ -114,4 +121,3 @@ test.describe.only('Handling Custom Dropdowns', () => {
     })
 
 })
-    
